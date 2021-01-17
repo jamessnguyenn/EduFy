29164a0bb83f09addfade0398ca17eb4ab5216b7
James Nguyen <nguyen.james615@gmail.com>
2021-01-16 16:40:48 -0800

    finished landing page

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -10,20 +10,34 @@ function landingPage() {
     return (
         <div className="about">
             <NavBar />
+            <div className="banner">
+                <div className="banner-container">
+                    <h1 className="bannerTitle"> Eudfy - Education For You</h1>
+                    <Link to="/signup">
+                        <button className="join-button"> Join now </button>
+                    </Link>
+                </div>
+            </div>
             <div className="hook-section">
-                <div className="flexCenterCol sloganText">
+                <div className="sloganText">
                     <h1 className="main-title">
-                        Plan Your Tasks.
-              <br />
-              Accomplish Goals. <br /> Earn Prizes.
+                        Motivation for Your Education
             </h1>
 
                     <div className="blueLine"></div>
                     <div className="prize-image">
+                    <p className="edufyDef">
+                              Edufy is EDU-cation For You! We're not just any <br/> other productivity application. 
+                              Our productivity app was made  <br/>to not only provide motivation but to also allow you <br/>
+                               to celebrate your achievements.<br/>
+                               <br/> 
+
+                    </p>
                         <img
                             src={newIdeasSVG}
                             className="newIdeasSVG"
                             alt="New Ideas Graphic" />
+                            
                     </div>
                 </div>
                 <div className="second-column">
@@ -39,7 +53,7 @@ function landingPage() {
                                 At Edufy, we believe students and teachers <br/>  
                                 deserve  to be celebrated for their accomplishments.<br/>
                                 That's why we created this platform for you to build <br/>
-                                goals get motivated, achieve  your goals<br/>
+                                goals, get motivated, and achieve  your dreams.<br/>
                                 
 
                     </p>
@@ -48,24 +62,40 @@ function landingPage() {
                 </div>
             </div>
 
+            <div className="explanation-container">
+            <div className="explanation">
+                            <div>
+                                <h1 className="about-title"> How Does it Work? </h1>
+                                <div className="blueLine"></div>
+                                <p className="edufyDef">
+                                You first start off with 100 health and 0 gold. Every time you finish a task, you gain 10 coins! <br/>
+                                If you don't finish the task, your health will dramatically drop. Coins you have earned can be   <br/>
+                                spent. Your coins show how hard you've hard so make sure you spend it correctlyat our store.
+                                <br/>  At 0 health, your profile wil fully  reset, so make sure to get those tasks going!  <br/>
+                                </p>
+                            </div>
+                           
+                    </div>
 
+
+            </div>
 
             <div className="directions">
                 <div className="dirContainer">
                     <div className="circleDir">1</div>
-                    <small className="dirSmall"> Apply </small>
+                    <small className="dirSmall">&nbsp;Build Goals&nbsp;&nbsp; </small>
                 </div>
                 <div className="dirContainer">
                     <div className="circleDir">2</div>
-                    <small className="dirSmall"> Match </small>
+                    <small className="dirSmall">&nbsp;&nbsp; Get Motivated&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</small>
                 </div>
                 <div className="dirContainer">
                     <div className="circleDir">3</div>
-                    <small className="dirSmall"> Build </small>
+                    <small className="dirSmall"> &nbsp;&nbsp;Win Rewards </small>
                 </div>
                 <div className="dirContainer">
                     <div className="circleDir">4</div>
-                    <small className="dirSmall"> Demo </small>
+                    <small className="dirSmall">&nbsp;&nbsp; Edufy and Repeat </small>
                 </div>
             </div>
 
@@ -74,7 +104,7 @@ function landingPage() {
                 <div className="footer-container">
                     <h1 className="footerTitle"> Edufy your life now </h1>
                     <Link to="/signup">
-                        <button className="BTNsignUp footerBTN"> Join now </button>
+                        <button className="footer-button"> Join now </button>
                     </Link>
                 </div>
             </div>
