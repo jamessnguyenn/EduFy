76431547297625dadf54112e2cd5e02027706000
James Nguyen <nguyen.james615@gmail.com>
2021-01-20 23:49:48 -0800

    made nav bar stick to top

--- src/components/unloggedNavBar/unloggedNavBar.jsx
+++ src/components/unloggedNavBar/unloggedNavBar.jsx
@@ -5,18 +5,22 @@ import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 
 import {Link} from'react-router-dom';
 
+const scrollToTop = (e)=>{
+  window.scrollTo(0,0);
+}
+
 export default function navbar() {
   return (
     <div className="unloggednavBar">
-      <Link to="/">
-      <h1 className="navbar-Title"> EduFy </h1>
-      </Link>
+      
+      <h1 className="navbar-Title" onClick={scrollToTop}> EduFy </h1>
       <div className="unloggednavEnd">
       <Link to="/login">
        <small className="unloggedNavLink">Login</small>
        </Link>
+       <small className="unloggedNavLink">|</small>
        <Link to="/signup">
-       <button className="sign-up-button">Sign Up <FontAwesomeIcon icon={faArrowRight}/></button>
+       <button className="sign-up-button">Sign Up &nbsp;<FontAwesomeIcon icon={faArrowRight}/></button>
        </Link>
       </div>
     </div>
